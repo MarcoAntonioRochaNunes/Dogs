a79f2cde204e7a790ad4a6c0a98ad44ea45fc715
Marco - Home <marco.antonio.rocha.nunes@hotmail.com>
2026-03-11 16:08:28 -0300

    .

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import styles from './PhotoContent.module.css'
 import { UserContext } from '../../UserContext';
-
+import PhotoCommentsForm from './PhotoCommentsForm';
 const PhotoComments = ({id, comments}) => {
   const {login} = React.useContext(UserContext);
   return (
     <div className={styles.comments}>
-      testesss
-    </div>
+        {login && <PhotoCommentsForm id={id} />}
+    </div>  
   )
 }
 
--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const PhotoCommentsForm = () => {
+const PhotoCommentsForm = ({id}) => {
   return (
-    <div>PhotoCommentsForm</div>
+    <div>PhotoCommentsForm {id}</div>
   )
 }
 
